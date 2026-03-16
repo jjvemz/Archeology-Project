245da07cefcbb23e42a626d90bcb92d1f42cfe79
vergarajj <vergarajj@hitss.com>
2026-03-16 20:03:08 -0300

    adding analitycs

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/hooks/useLanguage";
+import { Analytics } from '@vercel/analytics/react';
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -22,9 +23,11 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
+  
 );
 
 export default App;
